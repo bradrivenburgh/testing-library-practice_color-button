55195226fe847011eff6c97f2079eda016a095e1
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-03-04 12:59:39 -0500

    Test and code button color

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import "./App.css";
 
 function App() {
-  const [color, setColor] = useState('red');
-  const [text, setText] = useState('Change to blue')
-  const handleClick = (e) => {
-    const buttonSwitch = {
-      red: {
-        color: 'blue',
-        text: 'Change to red'
-      },
-      blue: {
-        color: 'red',
-        text: 'Change to blue'
-      }
-    }
-    setColor(buttonSwitch[e.target.style.backgroundColor].color);
-    setText(buttonSwitch[e.target.style.backgroundColor].text)
-  };
-
+  const [buttonColor, setButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
     <div>
       <button 
-        style={{ backgroundColor: color }}
-        onClick={(e) => handleClick(e)}>
-          {text}
+        style={{ backgroundColor: buttonColor }}
+        onClick={() => setButtonColor(newButtonColor)}> 
+          Change to {newButtonColor}
       </button>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ test("button has correct initial color", () => {
   render(<App />);
   
   // find an element with a role of button and text of 'Change to blue'
-  const colorButton = screen.getByRole("button", { name: /change to blue/i });
+  const colorButton = screen.getByRole("button", { name: 'Change to blue' });
   
   // expect the background color to be red
   expect(colorButton).toHaveStyle({ backgroundColor: "red" });
